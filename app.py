edf9731ac294b45477274b8fefbf0e5d66d731cf
Enzyss <62568159+Enzyss@users.noreply.github.com>
2024-04-15 22:22:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ async def verify(request: Request, phone_code: str = Form(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8023)
+    uvicorn.run(app, host="0.0.0.0")
